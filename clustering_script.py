0b67ed924c6d26ebcbeb96df15dd1f475856fe49
Jurand Pradzynski <jurandpradzynski@gmail.com>
2022-12-13 18:15:04 +0100

    visualization script commit

--- clustering_script.py
+++ clustering_script.py
@@ -37,22 +37,20 @@ filename = "alerts.csv"
 alerts = pd.read_csv(filename, index_col=0)
 alerts_classes = alerts[' Class']
 alerts_classes = alerts_classes.loc[alerts_classes != 'unknown']
-alerts_classes = alerts_classes.groupby(alerts_classes).count()
-alerts_classes = alerts_classes.loc[alerts_classes > 100]
-
+to_print = alerts_classes.groupby(alerts_classes).count()
 print('POPULATION OF PARTICULAR CLASSES')
-print(alerts_classes)
+print(to_print.loc[to_print > 100])
 
 ####################################################
 
 df_results = pd.DataFrame()
 
-# print('Make K-Means')
-# df_kmeans = cl.make_kmeans(df, n_clusters=n_clusters)
-# df_results['K-Means'] = df_kmeans
-# print(addtools.make_metrics(alerts_classes, df_kmeans))
-# with open(resultfilename + '.csv', 'a') as input:
-#     input.write('\n' + 'K-Means' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
+print('Make K-Means')
+df_kmeans = cl.make_kmeans(df, n_clusters=n_clusters)
+df_results['K-Means'] = df_kmeans
+print(addtools.make_metrics(alerts_classes, df_kmeans))
+with open(resultfilename + '.csv', 'a') as input:
+    input.write('\n' + 'K-Means' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
 
 print('Make DBSCAN')
 df_kmeans = cl.make_dbscan(df, n_jobs=n_jobs)
@@ -62,52 +60,48 @@ with open(resultfilename + '.csv', 'a') as input:
     input.write('\n' + 'DBScan' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
 
 
-# print('Make Mini Batch KMeans')
-# df_kmeans = cl.make_mini_batch_kmeans(df, n_clusters=n_clusters)
-# df_results['MiniBarchKMeans'] = df_kmeans
-# print(addtools.make_metrics(alerts_classes, df_kmeans))
-# with open(resultfilename + '.csv', 'a') as input:
-#     input.write('\n' + 'Mini Batch KMeans' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
+print('Make Mini Batch KMeans')
+df_kmeans = cl.make_mini_batch_kmeans(df, n_clusters=n_clusters)
+df_results['MiniBarchKMeans'] = df_kmeans
+print(addtools.make_metrics(alerts_classes, df_kmeans))
+with open(resultfilename + '.csv', 'a') as input:
+    input.write('\n' + 'Mini Batch KMeans' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
 
-# print('Make Bisecting KMeans')
-# df_kmeans = cl.make_bisecting_kmeans(df, n_clusters=n_clusters)
-# df_results['BisectingKMeans'] = df_kmeans
-# print(addtools.make_metrics(alerts_classes, df_kmeans))
-# with open(resultfilename + '.csv', 'a') as input:
-#     input.write('\n' + 'Bisecting KMeans' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
+print('Make Bisecting KMeans')
+df_kmeans = cl.make_bisecting_kmeans(df, n_clusters=n_clusters)
+df_results['BisectingKMeans'] = df_kmeans
+print(addtools.make_metrics(alerts_classes, df_kmeans))
+with open(resultfilename + '.csv', 'a') as input:
+    input.write('\n' + 'Bisecting KMeans' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
 
-# print('Make Agglomerative Clustering Avarage')
-# df_kmeans = cl.make_agglomerative_clustering(df, n_clusters = n_clusters, linkage = 'average')
-# df_results['AggCluAvarage'] = df_kmeans
-# print(addtools.make_metrics(alerts_classes, df_kmeans))
-# with open(resultfilename + '.csv', 'a') as input:
-#     input.write('\n' + 'Agglomerative Clustering Avarage' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
+print('Make Agglomerative Clustering Avarage')
+df_kmeans = cl.make_agglomerative_clustering(df, n_clusters = n_clusters, linkage = 'average')
+df_results['AggCluAvarage'] = df_kmeans
+print(addtools.make_metrics(alerts_classes, df_kmeans))
+with open(resultfilename + '.csv', 'a') as input:
+    input.write('\n' + 'Agglomerative Clustering Avarage' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
 
-# print('Make Agglomerative Clustering Complete')
-# df_kmeans = cl.make_agglomerative_clustering(df, n_clusters = n_clusters, linkage = 'complete')
-# df_results['AggCluComplete'] = df_kmeans
-# print(addtools.make_metrics(alerts_classes, df_kmeans))
-# with open(resultfilename + '.csv', 'a') as input:
-#     input.write('\n' + 'Agglomerative Clustering Complete' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
+print('Make Agglomerative Clustering Complete')
+df_kmeans = cl.make_agglomerative_clustering(df, n_clusters = n_clusters, linkage = 'complete')
+df_results['AggCluComplete'] = df_kmeans
+print(addtools.make_metrics(alerts_classes, df_kmeans))
+with open(resultfilename + '.csv', 'a') as input:
+    input.write('\n' + 'Agglomerative Clustering Complete' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
 
-# print('Make Aglomerative Clustering Single')
-# df_kmeans = cl.make_agglomerative_clustering(df, n_clusters = n_clusters, linkage = 'single')
-# df_results['AggCluSingle'] = df_kmeans
-# print(addtools.make_metrics(alerts_classes, df_kmeans))
-# with open(resultfilename + '.csv', 'a') as input:
-#     input.write('\n' + 'Agglomerative Clustering Single' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
+print('Make Aglomerative Clustering Single')
+df_kmeans = cl.make_agglomerative_clustering(df, n_clusters = n_clusters, linkage = 'single')
+df_results['AggCluSingle'] = df_kmeans
+print(addtools.make_metrics(alerts_classes, df_kmeans))
+with open(resultfilename + '.csv', 'a') as input:
+    input.write('\n' + 'Agglomerative Clustering Single' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
 
-# print('Make Agglomerative Clustering Ward')
-# df_kmeans = cl.make_agglomerative_clustering(df, n_clusters=n_clusters, linkage='ward')
-# df_results['AggCluWard'] = df_kmeans
-# print(addtools.make_metrics(alerts_classes, df_kmeans))
-# with open(resultfilename +'.csv', 'a') as input:
-#     input.write('\n' + 'Agglomerative Clustering Ward' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
-#     input.write('\n' + '\n')
+print('Make Agglomerative Clustering Ward')
+df_kmeans = cl.make_agglomerative_clustering(df, n_clusters=n_clusters, linkage='ward')
+df_results['AggCluWard'] = df_kmeans
+print(addtools.make_metrics(alerts_classes, df_kmeans))
+with open(resultfilename +'.csv', 'a') as input:
+    input.write('\n' + 'Agglomerative Clustering Ward' + '\n' + str(addtools.make_metrics(alerts_classes, df_kmeans)) )
+    input.write('\n' + '\n')
 
-print(df_results)
-df_results = pd.concat([df_results, alerts[' Class']], axis=1).reindex(df_results.index)
+df_results = pd.concat([df_results, alerts[' Class']], axis=0).reindex(df_results.index)
 df_results.to_csv(resultfilename + '_dataframe.csv')
-
-
-# print(df_results.groupby(['DBSCAN', 'Class']).count())
--- visualization_script.py
+++ visualization_script.py
@@ -1,13 +1,31 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn import metrics
 from clustering.additional_tools import read_json
 
+classes_labels = ['AGN', 'BL Lac', 'CV', 'QSO', 'SN II', 'SN IIn', 'SN Ia', 'YSO']
+methods = ['DBSCAN']
+
+def label_funct(name):
+    i = 0
+    for j in classes_labels:
+        i+=1
+        if name == j:
+            return i 
+    return 0
+
 config = read_json('configs/config.json')
 df = pd.read_csv(config['result_name'] + '_dataframe.csv')
+df['class_label'] = df[' Class'].apply(label_funct)
+df = df.loc[df['class_label'] != 0]
 
 print(df)
 
-for i in df.index:
-    print(i)
+for i in methods:
+    confusion_matrix = metrics.confusion_matrix(df['class_label'], df[i])
+    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix)
+    cm_display.plot()
+    plt.title(i)
+    plt.show()
