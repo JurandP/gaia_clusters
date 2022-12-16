be777f23447a96d81159709b0123a35a8b2acdaf
Jurand <jpradzynski@crunch05.astrouw.edu.pl>
2022-12-16 12:40:08 +0100

    Final confusion matrices png commit

--- visualization_script.py
+++ visualization_script.py
@@ -9,7 +9,7 @@ classes_labels = ['AGN', 'BL Lac', 'CV', 'QSO', 'SN II', 'SN IIn', 'SN Ia', 'YSO
 methods = ['DBSCAN', 'K-Means', 'MiniBatchKMeans', 'BisectingKMeans', 'AggCluAvarage', 'AggCluWard', 'AggCluSingle']
 
 def label_funct(name):
-    i = 1
+    i = 0
     for j in classes_labels:
         i+=1
         if name == j:
@@ -24,7 +24,7 @@ df = df.loc[df['class_label'] != 0]
 print(df)
 
 for i in methods:
-    confusion_matrix = metrics.confusion_matrix(df['class_label'], df[i], labels=[1,2,3,4,5,6,7,8])
+    confusion_matrix = metrics.confusion_matrix(df['class_label'], df[i], labels=[0,1,2,3,4,5,6,7])
     cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix)
     cm_display.plot()
     plt.title(i)
